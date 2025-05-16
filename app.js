4fe22a20ba582553b836c26bee2a90918ea452eb
syedmurtazanajaf <syedmurtaza545@gmail>
2025-05-16 17:12:49 +0500

    changes in home page

--- app.js
+++ app.js
@@ -4,3 +4,34 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener('click', () =>{
     mobileMenu.classList.toggle('show');
 });
+
+// navbar end
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const appearOptions = {
+    threshold: 0.2,
+    rootMargin: "0px 0px -10% 0px"
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible"); // Scroll out hone par remove
+      }
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => appearOnScroll.observe(fader));
+});
+
+
+
+
+
+
+
+
